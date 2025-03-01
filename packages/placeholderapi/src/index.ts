cae4e9b3c3b6cc7992931936a1745849f2180a04
Bruno Yuri Jacinto <bruno.orle@gmail.com>
2025-03-01 00:29:51 -0300

    checkpoint

--- packages/placeholderapi/src/index.ts
+++ packages/placeholderapi/src/index.ts
@@ -7,11 +7,3 @@ export async function GetUserById(id: number): Promise<any> {
 export async function GetUsers(): Promise<any> {
   return (await axios.get(`https://jsonplaceholder.typicode.com/users`)).data
 }
-
-export async function GetPostsByUserId(id: number): Promise<any> {
-  return (await axios.get(`https://jsonplaceholder.typicode.com/posts?userId=${id}`)).data
-}
-
-export async function GetCommentsByPostId(id: number): Promise<any> {
-  return (await axios.get(`https://jsonplaceholder.typicode.com/comments?postId=${id}`)).data
-}
